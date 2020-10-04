bf2d01af9e001bd5bedac95e773ee991473b7072
Maxdesir <39827919+Maxdesir@users.noreply.github.com>
2020-10-04 18:31:32 -0400

    Add files via upload

---
+++ Maxfunction.py
@@ -0,0 +1,15 @@
+# import matplotlib.pyplot as plt
+# year =[1950,1970,1990,1910]
+# pop =[2.519,3.692,5.263,6.972]
+# plt.plot(year,pop)
+# plt.xlabel('Maxinna')
+# plt.ylabel('The great population')
+# plt.show()
+# import matplotlib.pyplot as plt
+# plt.plot([1, 2, 3, 4])
+# plt.xlabel('The stupid population')
+# plt.show()
+a =input('what is this?')
+if a == yeso:
+	print('okay')yeso
+
